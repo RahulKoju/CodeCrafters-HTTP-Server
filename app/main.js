a02ffa8eb2f61707025c1137a45ecabe49965510
RahulKoju <blcraz75@gmail.com>
2024-07-17 18:23:53 +0545

    pass stage

--- app/main.js
+++ app/main.js
@@ -4,7 +4,16 @@ const net = require("net");
 
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data",(data)=>{
+    const req=data.toString();
+    if(req.startsWith("GET /")){
+      const httpResponse="HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(httpResponse);
+    }else{
+      const httpResponse="HTTP/1.1 404 NOT FOUND\r\n\r\n";
+      socket.write(httpResponse);
+    }
+  });
   socket.on("close", () => {
     socket.end();
   });
