3a899d6f46944a94cba36843949d5199927e393c
RahulKoju <blcraz75@gmail.com>
2024-07-17 18:34:43 +0545

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data",(data)=>{
     const req=data.toString();
-    if(req.startsWith("GET / ")){
+    if(req.startsWith("GET / HTTP/1.1")){
       const httpResponse="HTTP/1.1 200 OK\r\n\r\n";
       socket.write(httpResponse);
     }else{
